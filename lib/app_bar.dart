2633afd0f7af76e0c7d169f743e85d84846ac29d
Hafizuddin Sharif <hafizuddinshariff@gmail.com>
2020-05-30 15:29:40 +0800

    fixed and renamed appBar to topBar

--- lib/app_bar.dart
+++ lib/app_bar.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
 
-class NavBar extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return PreferredSize(
-      preferredSize: Size.fromHeight(100.0),
-      child: AppBar(
-        title: Text("UnIsolated", style:TextStyle(color: Colors.white)),
-        backgroundColor: Colors.cyan[800],
-      ),
-    );
-  }
+Widget topBar() {
+  return PreferredSize(
+    preferredSize: Size.fromHeight(40.0),
+    child: AppBar(
+      centerTitle: true,
+      title: Text("UnIsolated", style:TextStyle(color: Colors.black)),
+      backgroundColor: Colors.white,
+    ),
+  );
 }
--- lib/main_menu.dart
+++ lib/main_menu.dart
@@ -1,18 +1,12 @@
 import 'package:flutter/material.dart';
 import 'sidebar.dart';
+import 'app_bar.dart';
 
 class MainMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(40.0),
-        child: AppBar(
-          centerTitle: true,
-          title: Text("UnIsolated", style:TextStyle(color: Colors.black)),
-          backgroundColor: Colors.white,
-        ),
-      ),
+      appBar: topBar(),
       drawer: SideBar(),
       body: Container(),
     );
