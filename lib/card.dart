abbc05dc0781ffc851af249a117eb35a2ccd4a0d
kmhalpin <kmhapinn2@gmail.com>
2020-05-30 13:30:44 +0700

    first init to branch

---
+++ lib/card.dart
@@ -0,0 +1,69 @@
+
+import 'package:flutter/material.dart';
+
+class MyStatelessWidget extends StatelessWidget {
+  MyStatelessWidget({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        elevation: 6.0,
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(20.0))
+        ),
+        child: Container(
+          width: 300,
+          height: 120,
+          padding: EdgeInsets.only(top: 12.0),
+          decoration: BoxDecoration(
+            color: Colors.red,
+            borderRadius: BorderRadius.circular(20.0)
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Flexible(
+                flex: 0,
+                child: Container(
+                  alignment: Alignment(-0.9, 0),
+                  child: Text(
+                    "TYPE",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(fontSize: 16),
+                  ),
+                )
+              ),
+              Flexible(
+                flex: 1,
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                          bottomRight: Radius.circular(20.0),
+                          bottomLeft: Radius.circular(20.0)
+                      )
+                  ),
+                  child: Column(
+                    children: <Widget>[
+                      Text(
+                        "TIME",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 16, height: 2),
+                      ),
+                      Text(
+                        "ACTIVITY NAME",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 25),
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
 
+import 'card.dart';
+
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  static const String titles = "UnisolatedApp";
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      title: titles,
+      home: Scaffold(
+        appBar: AppBar(title: const Text(titles)),
+        body: MyStatelessWidget(),
+      ),
+    );
   }
 }
